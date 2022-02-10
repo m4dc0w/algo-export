24fed253aba3e7bc19e100ab457b9f11c804fda1
m4dc0w <m4dc0w@users.noreply.github.com>
2022-02-09 22:22:09 -0800

    Export tx for airdrops, applications, and mining.
    Airdrops:
    - Freckle Token airdrop tool
    - YLDY Team Airdrops
    
    Applications:
    - Tinyman Swaps and Liquidity Pools
    
    Mining:
    - PlanetWatch mining

---
+++ exporter/mining.go
@@ -0,0 +1,23 @@
+package exporter
+
+import (
+	"fmt"
+)
+
+// MiningPlanets exports transactions as an airdrop based on certain criteria.
+func MiningPlanets(records []ExportRecord) ([]ExportRecord, error) {
+	// Assume Mining transactions are usually done in 1 ASA deposit transaction.
+	if len(records) != 1 || !records[0].IsASAMining(27165954) {
+		return records, fmt.Errorf("invalid MiningPlanets() record")
+	}
+
+	r := records[0]
+
+	// https://explorer.planetwatch.io/
+	// PlanetWatch mining reward address ZW3ISEHZUHPO7OZGMKLKIIMKVICOUDRCERI454I3DB2BH52HGLSO67W754.
+	// ASA 27165954 (PLANET)
+	if r.sender == "ZW3ISEHZUHPO7OZGMKLKIIMKVICOUDRCERI454I3DB2BH52HGLSO67W754" && r.sender != r.account {
+		records[0].mining = true
+	}
+	return records, nil
+}
