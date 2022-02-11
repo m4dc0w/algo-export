a016d1707c43cfb27a18ce63b89d4e8073289056
m4dc0w <m4dc0w@users.noreply.github.com>
2022-02-10 19:16:44 -0800

    Update mining.go

--- exporter/mining.go
+++ exporter/mining.go
@@ -7,7 +7,7 @@ import (
 // MiningPlanets exports transactions as an airdrop based on certain criteria.
 func MiningPlanets(records []ExportRecord) ([]ExportRecord, error) {
 	// Assume Mining transactions are usually done in 1 ASA deposit transaction.
-	if len(records) != 1 || !records[0].IsASAMining(27165954) {
+	if !IsLengthExcludeReward(records, 1) || !records[0].IsASAMining(27165954) {
 		return records, fmt.Errorf("invalid MiningPlanets() record")
 	}
 
