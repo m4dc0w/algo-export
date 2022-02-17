048f3ded441678731249ebf13c46dc9c912ddddf
m4dc0w <m4dc0w@users.noreply.github.com>
2022-02-16 19:51:42 -0800

    Add DEFLY token.

--- exporter/verified_asa.go
+++ exporter/verified_asa.go
@@ -5,12 +5,13 @@ package exporter
 // Token disambiguation is needed when there are multiple coins with the same name.
 // https://cointracking.info/coin_charts.php
 var verifiedASA = map[uint64]string{
-	163650: "",         // ARCC
-	31566704: "",       // USDC
-	137594422: "",      // HDL
-	226701642: "",      // YLDY
-	230946361: "GEMS3", // Algogems
-	27165954: "",       // PLANETS
-	283820866: "",      // XET
-	287867876: "",      // OPUL
+	163650: "",          // ARCC
+	31566704: "",        // USDC
+	137594422: "",       // HDL
+	226701642: "",       // YLDY
+	230946361: "GEMS3",  // Algogems
+	27165954: "",        // PLANETS
+	283820866: "",       // XET
+	287867876: "",       // OPUL
+	470842789: "DEFLY2", // DEFLY
 }
