0002be66924ef111f56c256ffdde5b44292f3a1c
m4dc0w <m4dc0w@users.noreply.github.com>
2022-02-16 19:47:42 -0800

    Move verifiedASA to verified_asa.go

--- exporter/exporter.go
+++ exporter/exporter.go
@@ -18,20 +18,6 @@ type ExportFactory func() Interface
 
 var formats = map[string]ExportFactory{}
 
-// Verified ASA that export well to tax tracker websites (i.e. cointracking.info).
-// The string value allows for token disambiguation to the tax software.
-// Token disambiguation is needed when there are multiple coins with the same name.
-// https://cointracking.info/coin_charts.php
-var verifiedASA = map[uint64]string{
-	163650: "",         // ARCC
-	31566704: "",       // USDC
-	226701642: "",      // YLDY
-	230946361: "GEMS3", // Algogems
-	27165954: "",       // PLANETS
-	283820866: "",      // XET
-	287867876: "",      // OPUL
-}
-
 func registerFormat(format string, factory ExportFactory) {
 	formats[format] = factory
 }
