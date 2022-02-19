dc8e9fd7e71b4b9e3434c33fb370c84b608f16ee
m4dc0w <m4dc0w@users.noreply.github.com>
2022-02-18 20:43:28 -0800

    Update app_algofi.go

--- exporter/app_algofi.go
+++ exporter/app_algofi.go
@@ -6,6 +6,36 @@ import (
 
 // https://docs.algofi.org/protocol/mainnet-contracts
 // https://cointracking.freshdesk.com/en/support/solutions/articles/29000033408-loans-and-their-repayments
-func ApplAlgoFi(records []ExportRecord,  txns []models.Transaction) ([]ExportRecord, error) {
-	return records, nil
+
+// https://app.algofi.org/
+func ApplAlgoFiMarket(records []ExportRecord, txns []models.Transaction) ([]ExportRecord, error) {
+	onCompletion, action := ExtractFirstArg(txns)
+
+	// Supply
+	if action == "mt" {
+		records[0].comment = "AlgoFi - Supply"
+		return records, nil
+	}
+
+	// Withdraw
+	if action == "rcu" {
+		records[0].comment = "AlgoFi - Withdraw"
+		return records, nil
+	}
+
+	// Borrow
+	if action == "b" {
+		records[0].incomeNoTax = true
+		records[0].comment = "AlgoFi - Borrow"
+		return records, nil
+	}
+
+	// Repay
+	if action == "rb" {
+		records[0].expenseNoTax = true
+		records[0].comment = "AlgoFi - Repay"
+		return records, nil
+	}
+
+	return records, fmt.Errorf("invalid ApplAlgoFiMarket() record | onCompletion: %s | action: %s | records length: %d | txns length: %d\n", onCompletion, action, len(records), len(txns))
 }
